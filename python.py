829da07fe3c0b377089f767dd2d7933bd9d95414
Gabriel de Brito Abreu <168465951+notebookpositivo@users.noreply.github.com>
2025-03-24 08:41:50 -0300

    att1

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
 nome = str(input('Qual seu nome? '))
+idade = int(input('Qual sua idade? '))
+
+if (idade<16) = 'Nao precisa votar'
+
+elif (idade == 16 | idade > 65) = 'Voto não obrigatorio'
+
+else = 'Voto obrigatorio'
