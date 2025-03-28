96cdfa9fea97d94bf63aa4a256cd732722e0ab70
Gabriel de Brito Abreu <168465951+notebookpositivo@users.noreply.github.com>
2025-03-28 11:21:32 -0300

    upd1

---
+++ sistema_banco.py
@@ -0,0 +1,12 @@
+def menu():
+    print('''
+        *******Menu*******
+        (1) Criar Usuário
+        (2) Criar Conta
+        (3) Listar contas
+        (d) Depositar
+        (s) Sacar
+        (e) Extrato
+        (q) Sair
+    ''')
+    return input('Qual opção deseja?')
