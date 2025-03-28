b8ff612fb1aca2047828ba6bee17bb8db1cba151
Gabriel de Brito Abreu <168465951+notebookpositivo@users.noreply.github.com>
2025-03-28 12:18:20 -0300

    upd3

--- sistema_banco.py
+++ sistema_banco.py
@@ -1,4 +1,5 @@
 saldo = 0
+limite_saldo = 500
 numero_saques = 0 
 limites_saques = 3
 usuarios=[] #lista vazia para adicionar valores futuramente
@@ -33,5 +34,20 @@ def deposito(valor,saldo):
 
     return (saldo)
 
-deposito (90,0)
+
+def saque (saque,saldo):
+    if numero_saques >= limites_saques:
+        print('Você atingiu o limite de saques da sua conta, tente novamente em outro dia útil')
+    else:
+        if saque <= 0 :
+            print('Saque invalido. Verifique o valor e tente novamente.')
+        elif saque > limite :
+            print('Valor limite excedido. Verifique o valor e tente novamente.')
+        elif saque > saldo :
+            print('Saldo insuficiente. Verifique o valor e tente novamente.')
+        else:
+            saldo -= saque
+            numero_saques +=1
+    return (saldo)            
+
 
