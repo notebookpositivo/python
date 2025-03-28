532919f55a08aba9f59aef6a2de5645ca28ed643
Gabriel de Brito Abreu <168465951+notebookpositivo@users.noreply.github.com>
2025-03-28 11:57:17 -0300

    upd2

--- sistema_banco.py
+++ sistema_banco.py
@@ -1,3 +1,11 @@
+saldo = 0
+numero_saques = 0 
+limites_saques = 3
+usuarios=[] #lista vazia para adicionar valores futuramente
+contas = []
+agencia='0001'
+
+
 def menu():
     print('''
         *******Menu*******
@@ -10,3 +18,20 @@ def menu():
         (q) Sair
     ''')
     return input('Qual opção deseja?')
+
+
+def deposito(valor,saldo):
+    # vou receber o valor do deposito e somar o valor ao saldo
+    if valor > 0 :
+        saldo +=valor
+        #print mostrando o valor depositado
+        print(f'Você depositou R${valor}')
+    
+    else:
+        print('Valor do deposito inválido. Verifique a quantia digitada.')
+
+
+    return (saldo)
+
+deposito (90,0)
+
