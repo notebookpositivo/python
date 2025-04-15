915b397b56ad8b6c19f6e656d67e198948d464c1
Gabriel de Brito Abreu <168465951+notebookpositivo@users.noreply.github.com>
2025-04-15 11:45:43 -0300

    Create calc1.py

---
+++ calc1.py
@@ -0,0 +1,22 @@
+def calculadora():
+    while True:
+        op = input("Operação (+, -, , %) ou 'sair': ")
+        if op.lower() == 'sair': break
+
+        if op in '+-%':
+            try:
+                x = float(input("1º número: "))
+                y = float(input("2º número: "))
+
+                if op == '+': r = x + y
+                elif op == '-': r = x - y
+                elif op == '': r = x y
+                elif op == '%': r = x % y if y != 0 else "Erro: Divisão por zero!"
+
+                print(f"Resultado: {x} {op} {y} = {r}")
+            except:
+                print("Digite números válidos!")
+        else:
+            print("Operação inválida!")
+
+calculadora()
